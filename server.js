66834d8f24e1573a76843126320c5098bc3f6a28
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2021-07-29 02:49:30 +0530

    .env changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ extended: false }));
 app.use(morgan("combined"));
 
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
